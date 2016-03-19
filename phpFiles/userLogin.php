827fa56eed6d6c5897ec7c5d5cc6ef7878e614f0
Tim Dickson <gteldirith543@gmail.com>
2016-03-19 00:02:53 -0500

    adding user page

---
+++ phpFiles/userLogin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/indexStyle.css">
+<script src="javascript/jquery-1.12.0.js"></script>
+</head>
+<body>
+<div id="div1"><h2>User Login Page</h2></div>
+</div>
+<footer id="footer">&copy; 2016 Dickson and Team LLC.</footer>
+</body>
+</html>
