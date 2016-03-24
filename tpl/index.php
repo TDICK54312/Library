7313e163322f9bcd75c5eb07fe1b699a8c4b9c48
xeagl3x <css_eagle@hotmail.com>
2016-03-23 22:33:56 -0500

    Created some template structure
    added menu and main structure

---
+++ tpl/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+ <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="style/menu-style.css">
+   <link rel="stylesheet" href="style/main.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="js/menu_script.js"></script><title>Library Database System</title>
+
+</head>
+
+<body>
+<div id="wrapper">
+<div id="header">head stuff</div>
+<div id="navarea">
+<div id='cssmenu'>
+<ul>
+   <li><a href='#'>Home</a></li>
+   <li class='active'><a href='#'>Products</a>
+      <ul>
+         <li><a href='#'>Product 1</a>
+            <ul>
+               <li><a href='#'>Sub Product</a></li>
+               <li><a href='#'>Sub Product</a></li>
+            </ul>
+         </li>
+         <li><a href='#'>Product 2</a>
+            <ul>
+               <li><a href='#'>Sub Product</a></li>
+               <li><a href='#'>Sub Product</a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'>About</a></li>
+   <li><a href='#'>Contact</a></li>
+</ul>
+</div></div>
+<div id="content">
+content here
+</div>
+<div id="footer">stuff</div>
+</div>
+</body>
+</html>
