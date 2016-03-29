72e2379bbf7848c793e29273233b3c6128d87397
Tim Dickson <gteldirith543@gmail.com>
2016-03-29 15:01:41 -0500

    adding a nav bar adding a nav bar

--- phpFiles/adminProfile.php
+++ phpFiles/adminProfile.php
@@ -4,15 +4,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
 <link rel="stylesheet" type="text/css" href="../css/adminLoginPage.css">
 </head>
 <body>
 <div id="hello">
 	<h1>Welcome <?php echo $_SESSION["admininfo"][5];?>, to the Admin Library Console</h1>
 </div>
-<div id="output">
-	<a href="phpFiles/adminCreateUser.php">Create User</a>
-	<a href="phpFiles/adminCheckInventory.php">Inventory</a>
+<div id='cssmenu'>
+	<ul>
+		<li class='active'><a href='#'><span>Home</span></a></li>
+		<li><a href='#'><span>Products</span></a></li>
+		<li><a href='#'><span>Company</span></a></li>
+		<li class='last'><a href='#'><span>Contact</span></a></li>
+	</ul>
 </div>
 </body>
 </html>
