88d31f8d26c2d72997dfcba156934530e4767ee8
Tim Dickson <gteldirith543@gmail.com>
2016-04-03 18:58:11 -0500

    adding some isbn stuff

--- phpFiles/adminAddBook.php
+++ phpFiles/adminAddBook.php
@@ -12,8 +12,21 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-   <!--<script src="../javascript/script.js"></script> -->
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <link rel="stylesheet" type="text/css" href="../css/adminLoginPage.css">
+<script>
+$(document).ready(function () {
+  //called when key is pressed in textbox
+  $("#isbnNumber").keypress(function (e) {
+     //if the letter is not digit then display error and don't type anything
+     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        //display error message
+        $("#errmsg").html("Digits Only").show().fadeOut("slow");
+               return false;
+    }
+   });
+});
+</script>
 </head>
 <body>
 <div id="hello">
@@ -22,7 +35,7 @@ session_start();
 <div id='addBookForm'>
 	<form method = "POST" action = "">
 	Book Title<br><input type="text" name="bookTitle" size="40"></br>
-	ISBN Number<br><input type="text" name="isbnNumber" size="40"></br>
+	ISBN Number<br><input type="text" name="isbnNumber" id="isbnNumber" size="40" maxlength="10"></br>
 	Author First Name<br><input type="text" name="authorFName" size="40"></br>
 	Author Last Name<br><input type="text" name="authorLName" size="40"></br>
 	Publisher<br><input type="text" name="publisher" size="40"></br>
