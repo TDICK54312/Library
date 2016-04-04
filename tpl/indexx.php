edae497d209abe68f9929d8a32ba2b4e3c92da05
Tim Dickson <gteldirith543@gmail.com>
2016-04-04 18:28:54 -0500

    adding indexx.php test page to add functionality to Albien's beautiful art.

---
+++ tpl/indexx.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+ <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="style/menu-style.css">
+   <link rel="stylesheet" href="style/main.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="js/menu_script.js"></script><title>Library Database System</title>
+   <script src="http://use.edgefonts.net/berkshire-swash;noticia-text.js"></script>
+
+</head>
+
+<body>
+<div id="wrapper">
+<div id="header"><img src="images/header.jpg" style="width: 100%;"></div>
+<div id="navarea" style="text-align: center; display: block; margin: 0 auto;">
+	<?php include_once('navmenu.php'); ?>
+</div>
+<div id="content">
+	<h1>Welcome to our Library!</h1>
+    <p> Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipiscing elit. Vestibulum in euismod nisl. Proin et ultrices est. Fusce mattis ligula a tellus dignissim, bibendum cursus magna aliquam. Fusce ullamcorper, sapien at dignissim dictum, odio purus placerat lorem, ac eleifend lectus ex vel urna. Vivamus porta accumsan tincidunt. Sed facilisis, erat eu fringilla lacinia, dui velit euismod lectus, tincidunt condimentum sem turpis in leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam convallis pharetra ligula vel maximus. Quisque sem nibh, aliquam non nibh scelerisque, fringilla finibus neque. Sed sagittis sagittis lorem a sagittis. Nulla eget enim sit amet erat commodo aliquet eu eu enim.
+</p><p>
+Fusce at iaculis urna. Cras sagittis, quam nec porttitor finibus, purus felis efficitur leo, non rhoncus urna turpis a ligula. Curabitur condimentum libero vel augue iaculis, ut imperdiet erat fringilla. Aenean id finibus neque. Nullam imperdiet maximus mollis. Cras aliquam molestie est id congue. Fusce congue nunc eget sodales vestibulum. Integer eget dictum velit. Donec consequat vel ipsum sed interdum. Proin ut diam at lacus lobortis tristique. Donec commodo tellus sed nunc malesuada, quis ullamcorper augue consequat. In maximus facilisis iaculis. </p>
+
+	<h1>New Additions</h1>
+	<div>
+    	<div class="book-cont">
+        	<img src="http://placehold.it/150x120.jpg"><br>
+            <a href="#">Book Title</a><br>
+            Publisher
+        </div>
+        <div class="book-cont">
+        	<img src="http://placehold.it/150x120.jpg"><br>
+            <a href="#">Book Title</a><br>
+            Publisher
+        </div>
+        <div class="book-cont">
+        	<img src="http://placehold.it/150x120.jpg"><br>
+            <a href="#">Book Title</a><br>
+            Publisher
+        </div>
+        <div class="book-cont">
+        	<img src="http://placehold.it/150x120.jpg"><br>
+            <a href="#">Book Title</a><br>
+            Publisher
+        </div>
+        <div class="book-cont">
+        	<img src="http://placehold.it/150x120.jpg"><br>
+            <a href="#">Book Title</a><br>
+            Publisher
+        </div>
+        <div class="book-cont">
+        	<img src="http://placehold.it/150x120.jpg"><br>
+            <a href="#">Book Title</a><br>
+            Publisher
+        </div>
+    </div>
+</div>
+<div id="footer">Copyright (c) 2016. Template by <a href="mailto:albien.fezga@live.com">Albien Fezga</a>.</div>
+</div>
+</body>
+</html>
