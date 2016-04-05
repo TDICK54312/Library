bde76218a01d7dc4e18795afa27f5595277aa4b4
Tim Dickson <gteldirith543@gmail.com>
2016-04-05 16:19:58 -0500

    wrong file.

--- tpl/navmenu.php
+++ tpl/navmenu.php
@@ -13,23 +13,6 @@
    		{
 			?>
    		<li><a href="#">Login</a></li>
-   		<div id="loginBox">                
-                    <form id="loginForm">
-                        <fieldset id="body">
-                            <fieldset>
-                                <label for="email">Email Address</label>
-                                <input type="text" name="email" id="email" />
-                            </fieldset>
-                            <fieldset>
-                                <label for="password">Password</label>
-                                <input type="password" name="password" id="password" />
-                            </fieldset>
-                            <input type="submit" id="login" value="Sign in" />
-                            <label for="checkbox"><input type="checkbox" id="checkbox" />Remember me</label>
-                        </fieldset>
-                        <span><a href="#">Forgot your password?</a></span>
-                    </form>
-                </div>
     <?php } else { ?>
     <li><a href="#">My Account</a></li>
     <li><a href="#">Logout</a></li>
