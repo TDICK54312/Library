5dffb2edefc3dd2efb67960d46f376c1d8b77feb
Tim Dickson <gteldirith543@gmail.com>
2016-04-05 17:27:19 -0500

    Setting up user login pages.

--- tpl/navmenuTest.php
+++ tpl/navmenuTest.php
@@ -14,6 +14,19 @@
 			?>
    		<li><a href="login.php">Login</a></li>
     <?php } else { ?>
+    <?php if($_SESSION['userinfo'][1] == 1){?>
+	    	<li><a href="#">Admin Functions</a>
+	    		<ul>
+		    		<li><a href="#">Check-In Book</a></li>
+		    		<li><a href="#">Check-Out Book</a></li>
+		    		<li><a href="#">Add User</a></li>
+		    		<li><a href="#">Add Book</a></li>
+		    		<li><a href="#">Delete User</a></li>
+		    		<li><a href="#">Delete Book</a></li>
+		    		<li><a href="#">Reports</a></li>
+	    		</ul>
+	    	</li>
+    <?php } ?> 
     <li><a href="#">My Account</a></li>
     <li><a href="#">Logout</a></li>
     <?php } ?>
