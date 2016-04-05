b669796b9ea5f19ce41a7ff5a73ed904c5ef9200
Tim Dickson <gteldirith543@gmail.com>
2016-04-05 18:22:55 -0500

    phrasing

--- tpl/adminProfile.php
+++ tpl/adminProfile.php
@@ -21,7 +21,7 @@
 	<?php include_once('navmenuTest.php'); ?>
 </div>
 <div id="content">
-	<h1>Welcome <i><?php echo $_SESSION["user"][1] . " " . $_SESSION["user"][2]; ?></i> you last logged in at <i><?php echo $_SESSION["userinfo"][5]; ?></i> !</h1>
+	<h1>Welcome <i><?php echo $_SESSION["user"][1] . " " . $_SESSION["user"][2]; ?></i> you last logged in on <i><?php echo $_SESSION["userinfo"][5]; ?></i> !</h1>
 </div>
 </div>
 </body>
