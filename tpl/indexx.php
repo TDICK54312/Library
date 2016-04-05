a8b864ee493b8f15554fd95929a8e237691bc120
Tim Dickson <gteldirith543@gmail.com>
2016-04-05 16:15:30 -0500

    Adding login dropdown stuff

--- tpl/indexx.php
+++ tpl/indexx.php
@@ -4,19 +4,18 @@
  <meta charset='utf-8'>
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="style/menu-style.css">
+   <link rel="stylesheet" href="style/menu-styleTest.css">
    <link rel="stylesheet" href="style/main.css">
    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <script src="js/menu_script.js"></script><title>Library Database System</title>
    <script src="http://use.edgefonts.net/berkshire-swash;noticia-text.js"></script>
-
 </head>
 
 <body>
 <div id="wrapper">
 <div id="header"><img src="images/header.jpg" style="width: 100%;"></div>
 <div id="navarea" style="text-align: center; display: block; margin: 0 auto;">
-	<?php include_once('navmenu.php'); ?>
+	<?php include_once('navmenuTest.php'); ?>
 </div>
 <div id="content">
 	<h1>Welcome to our Library!</h1>
@@ -61,4 +60,5 @@ Fusce at iaculis urna. Cras sagittis, quam nec porttitor finibus, purus felis ef
 <div id="footer">Copyright (c) 2016. Template by <a href="mailto:albien.fezga@live.com">Albien Fezga</a>.</div>
 </div>
 </body>
+<script src="js/login.js" type="text/javascript"></script>
 </html>
