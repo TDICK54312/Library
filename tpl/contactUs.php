6630febc60d32fe502f754e401631e09743b8cf3
xeagl3x <css_eagle@hotmail.com>
2016-04-10 16:48:10 -0500

    testing bsh

--- tpl/contactUs.php
+++ tpl/contactUs.php
@@ -7,6 +7,7 @@
 	<h1>Contact Us</h1>
 	<p> Email: team3LibraryApp@gmail.com </p>
 	<p> Phone: (800) 273-8255 </p>
+    <br>
 	<p> We would love to hear your feedback and if you have any issues with our software! </p>
 </div>
 <?php include_once('footer.php'); ?>
