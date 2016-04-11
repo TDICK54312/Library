53c88dca15e3eeadda1de7e0eac08a6d76cf765b
Tim Dickson <gteldirith543@gmail.com>
2016-04-10 23:13:25 -0500

    adding reports.

---
+++ tpl/viewReports.php
@@ -0,0 +1,12 @@
+<?php
+	include('api.php');
+	include_once('header.php');
+	
+?>
+<div id="content">
+	<h1>Run and View Report!</h1>
+	<a href="viewUsersReport.php">View Users</a>
+	<a href="viewTransactionsReport.php">View Transactions</a>
+	<a href="viewBooksReport">View Books</a>
+</div>
+<?php include_once('footer.php'); ?>
