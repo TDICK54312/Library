483857f3aee5e954bcbc4b7923c68f17c48b789e
Tim Dickson <gteldirith543@gmail.com>
2016-04-10 19:36:13 -0500

    stuff.

--- tpl/adminAddBook.php
+++ tpl/adminAddBook.php
@@ -34,7 +34,7 @@
 			echo "Summary not entered!";
 		}
 		if(!empty($_POST['image'])){
-			$target_file = basename($_FILES["image"]["name"]);
+			$target_file = basename($_FILES['image']['name']);
 			$uploadOk = 1;
 			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 			
@@ -56,26 +56,27 @@
 				$uploadOk = 0;
 			}*/
 			// Check file size
-			if ($_FILES["fileToUpload"]["size"] < 64000) {
-				//echo "Sorry, your file is too large.";
+			if ($_FILES['image']['size'] < 64000) {
+				echo "Sorry, your file is too large.";
 				$uploadOk = 0;
 			}
 				
 			// Allow certain file formats
 			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-				//echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+				echo "Sorry, only JPG, JPEG, PNG files are allowed.";
 				$uploadOk = 0;
 			}
 			
 			// Check if $uploadOk is set to 0 by an error
 			if ($uploadOk == 0) {
-				//echo "Sorry, your file was not uploaded.";
+				echo "Sorry, your file was not uploaded.";
 				
 				// if everything is ok, try to upload file
 			} 
 		}
 		if($everythingSet == true){
-			$result = addBook($_POST['isbn'], $_POST['authorfname'], $_POST['authorlname'], $_POST['pub'], $_POST['summary'], $_POST['genre'], $_POST['booktitle'],$_POST['image']);
+			$result = addBook($_POST['isbn'], $_POST['authorfname'], $_POST['authorlname'], $_POST['pub'], $_POST['summary'], $_POST['genre'], $_POST['booktitle'], $_POST['image']);
+			echo var_dump($_POST['image']);
 			echo $result;
 		}	
 	}
