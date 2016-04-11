93ac56c60bf07a5739ffcdb858547c65893356ad
xeagl3x <css_eagle@hotmail.com>
2016-04-10 19:49:56 -0500

    rearranged code

--- tpl/lookAtBook.php
+++ tpl/lookAtBook.php
@@ -1,9 +1,10 @@
 <?php
 	include 'api.php';
 	include_once('header.php');
-	
+?>
 
-	if(!empty($_POST['submit'])){
+<div id="content">
+<?php if(!empty($_POST['submit'])){
 		if($_SESSION["userinfo"][1] == 1 || $_SESSION["userinfo"][1] == 2 || $_SESSION["userinfo"][1] == 3){
 			$result = addToUserRental($_SESSION["userinfo"][0], $_POST['isbn']);
 		}
@@ -12,8 +13,6 @@
 		}
 	}
 ?>
-
-<div id="content">
 <h1><?php echo $_GET['title']; ?> </h1>
 	<?php
 		$aISBN = $_GET['isbn'];
