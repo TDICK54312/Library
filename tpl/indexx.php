01d45aab4496f3b04e3a4d1362a35d40a1ac247a
Tim Dickson <gteldirith543@gmail.com>
2016-04-10 22:48:48 -0500

    working on index again

--- tpl/indexx.php
+++ tpl/indexx.php
@@ -1,6 +1,20 @@
 <?php
 	include('api.php');
 	include_once('header.php');
+	
+	$everythingSet = true;
+	if(!empty($_POST['submit'])){
+		if(empty($_POST['isbn'])){
+			$everythingSet = false;
+			echo "ISBN Number not entered!";
+		}
+		else{
+			$theISBN = $_POST['isbn'];
+			$theTitle = $_POST['title'];
+			header("Location: lookAtBook.php?isbn=$theISBN");
+			exit;
+		}
+	}
 ?>
 
 <div id="content">
