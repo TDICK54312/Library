8c612b65b5e9009bb0bcb0081756f4a7685056be
xeagl3x <css_eagle@hotmail.com>
2016-04-11 18:38:34 -0500

    added search function

---
+++ tpl/inc_search.php
@@ -0,0 +1,8 @@
+<div id="search">
+	<form name="searchform" method="post" action="search.php">
+    	<input type="text" placeholder="search" width="150px" name="c">
+        <input type="submit" style="display: inline-block; margin-right: 3px;" name "tp" value="Title">
+        <input type="submit" style="display: inline-block; margin-right: 3px;" name "tp" value="Author">
+        <input type="submit" style="display: inline-block; margin-right: 3px;" name "tp" value="ISBN">
+    </form>
+</div>
---
+++ tpl/search.php
@@ -0,0 +1,32 @@
+<?php 
+
+	// search function
+	include_once('header.php');
+	include('api.php');	
+	
+	if($_POST['tp' ] == 'Title')
+		$type = 1;
+	else if($_POST['tp'] == 'Author')
+		$type = 2;
+	else if($_POST['tp'] == 'ISBN')
+		$type = 3;
+	else {
+		echo "Invalid input.";
+	}
+	
+	$content = htmlspecialchars($_POST['c']);
+	
+		
+
+?>
+<div id="content">
+	<?php 
+	if(isset($type) && isset($content))
+		search($type,$content);
+	else
+		echo "<p>Invalid search.</p>";
+		
+	 ?>
+
+</div>
+<?php include_once('footer.php'); ?>
