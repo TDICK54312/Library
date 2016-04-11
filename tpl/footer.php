ac19626b13ee6d590d519d9eaf575f1f6588ca56
Tim Dickson <gteldirith543@gmail.com>
2016-04-11 00:42:09 -0500

    changed template to say Team 3swag

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,4 +1,4 @@
-<div id="footer">Copyright (c) 2016. Template by <a href="mailto:albien.fezga@live.com">Albien Fezga</a>.</div>
+<div id="footer">Copyright (c) 2016. Template by <a href="mailto:albien.fezga@live.com">Team 3</a>.</div>
 </div>
 </body>
 </html>
