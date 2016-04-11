7b344019eb19d40cd00a707247bdcf0c85630a5b
Tim Dickson <gteldirith543@gmail.com>
2016-04-10 20:24:04 -0500

    checking FILE

--- tpl/adminAddBook.php
+++ tpl/adminAddBook.php
@@ -77,7 +77,7 @@
 		if($everythingSet == true && $uploadOk == 1){
 			$result = addBook($_POST['isbn'], $_POST['authorfname'], $_POST['authorlname'], $_POST['pub'], $_POST['summary'], $_POST['genre'], $_POST['booktitle'], $_FILES['image']['tmp_name']);
 			echo "<pre>";
-			echo print_r($_POST['image']);
+			echo print_r($_FILES['image']);
 			echo "</pre>";
 			echo $result;
 		}	
