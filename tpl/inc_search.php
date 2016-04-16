bb0985d7df138c3a07c5e712cabd61c74a1d676c
Tim Dickson <gteldirith543@gmail.com>
2016-04-15 20:37:30 -0500

    selector for search

--- tpl/inc_search.php
+++ tpl/inc_search.php
@@ -1,13 +1,10 @@
 <div id="search">
 	<form name="searchform" method="post" action="search.php">
-		<select name="searchType" id="searchRole" class="txtfield">
-					<option value="1">Title</option>
-					<option value="2">Author</option>
-					<option value="3">ISBN</option>
-				</select>
+		<select name="searchType" id="searchRole" class="txtfield" style="display: inline-block; margin-right: 3px;">
+			<option value="1">Title</option>
+			<option value="2">Author</option>
+			<option value="3">ISBN</option>
+		</select>
     	<input type="text" placeholder="search" width="150px" name="c">
-        <!--<input type="submit" style="display: inline-block; margin-right: 3px;" name="tp" value="Title">
-        <input type="submit" style="display: inline-block; margin-right: 3px;" name="tp" value="Author">
-        <input type="submit" style="display: inline-block; margin-right: 3px;" name="tp" value="ISBN"> -->
     </form>
 </div>
