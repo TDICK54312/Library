db7840ff8ac2c35491df202e1b04243d15b336ca
Tim Dickson <gteldirith543@gmail.com>
2016-04-20 16:36:05 -0500

    adding show rentals to user profile.

--- tpl/userProfile.php
+++ tpl/userProfile.php
@@ -2,13 +2,14 @@
 	
 	include_once('header.php');
 	include('inc_search.php');
+	include('api.php');
 ?>
 
 
 <div id="content">
-	<h1>Welcome <i><?php echo $_SESSION["user"][1] . " " . $_SESSION["user"][2]; ?></i> you last logged in on <i><?php echo $_SESSION["userinfo"][5]; ?></i> !</h1>
+	<p> <i><?php echo $_SESSION["user"][1] . " " . $_SESSION["user"][2]; ?></i> you last logged in on <i><?php echo $_SESSION["userinfo"][5]; ?></i> !</p>
 	<?php
-		//put current checked out books and stuff here. 
+		getRentedBooks($_SESSION["userinfo"][0], $_SESSION["userinfo"][6], $_SESSION["userinfo"][1]);
 		?>
 </div>
 <?php include_once('footer.php'); ?>
