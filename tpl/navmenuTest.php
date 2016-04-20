d8ac362fd86241d6569c40c4125caee0b44e9d8b
Tim Dickson <gteldirith543@gmail.com>
2016-04-20 16:38:20 -0500

    lol userprofile php file was not added to the menu for normal users.

--- tpl/navmenuTest.php
+++ tpl/navmenuTest.php
@@ -36,7 +36,7 @@ $(function(){
 	    	<li><a href="adminProfile.php">My Account</a></li>
     <?php } ?>
     <?php if($_SESSION["userinfo"][1] == 2 || $_SESSION["userinfo"][1] == 3){?> 
-    	<li><a href="#">My Account</a></li>
+    	<li><a href="userProfile.php">My Account</a></li>
     	<?php } ?>
     <li><a href="login.php" id="logout">Logout</a></li>
     <?php } ?>
