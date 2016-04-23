0177b90004f310f26aee4f074e15fd4c038a0252
Tim Dickson <gteldirith543@gmail.com>
2016-04-22 23:30:47 -0500

    styles

--- tpl/viewTransactionsReport.php
+++ tpl/viewTransactionsReport.php
@@ -3,6 +3,15 @@
 	include_once('header.php');
 	include('inc_search.php');
 ?>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+}
+</style>
 <div id="content">
 	<h1>Transactions</h1>
 	<?php
