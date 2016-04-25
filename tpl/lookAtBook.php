528a4e6b94ef169aef83e2bad9bc7967004eb9e5
stickerMan <tinchiv1@gmail.com>
2016-04-24 23:33:01 -0500

    minor changes

--- tpl/lookAtBook.php
+++ tpl/lookAtBook.php
@@ -13,7 +13,7 @@
 			}
 		}
 		else{
-			echo "<p>Please Login in order to add this book.</p>";
+			echo "<p><font color="red"> **Please Login in order to add this book.</font></p>";
 		}
 		if($_SESSION["userinfo"][6] == 0){
 			echo "You have checked out the max number of books please return your books!";
