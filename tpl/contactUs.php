216e29467787c9ae1b8f8a38745851887dc71dfb
stickerMan <tinchiv1@gmail.com>
2016-04-24 22:19:57 -0500

    fixed facebook button

--- tpl/contactUs.php
+++ tpl/contactUs.php
@@ -10,5 +10,6 @@
 	<p> Phone: (800) 273-8255 </p>
     <br>
 	<p> We would love to hear your feedback and if you have any issues with our software! </p>
+	<!-- Facebook Badge START --><a href="https://www.facebook.com/pages/Digital-Library/1173058809405785" title="Digital Library" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" target="_TOP">Digital Library</a><br /><a href="https://www.facebook.com/pages/Digital-Library/1173058809405785" title="Digital Library" target="_TOP"><img class="img" src="https://badge.facebook.com/badge/1173058809405785.3004.283613488.png" style="border: 0px;" alt="" /></a><br /><a href="https://www.facebook.com/advertising" title="Make your own badge!" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" target="_TOP">Promote Your Page Too</a><!-- Facebook Badge END -->
 </div>
 <?php include_once('footer.php'); ?>
